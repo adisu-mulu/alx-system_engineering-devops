c4951191d5d14ca5842a7308e42537874265acd1
adisu-mulu <kulhabesh31@gmail.com>
2024-07-04 14:00:09 +0300

    updating 1-install

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-# install flask
-package { 'pip3':
-  ensure => installed,
-}
-
-package {'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['pip3']
+#install flask
+package { 'Flask':
+ensure   => '2.1.0',
+provider => 'pip3',
 }
